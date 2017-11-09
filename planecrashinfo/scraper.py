d8b9137a99cb649b1862b32dc5d3510f4d277e22
Zuber <zuberahmed99@gmail.com>
2017-11-10 00:41:11 +0530

    added method to iterate through yearly data

--- planecrashinfo/scraper.py
+++ planecrashinfo/scraper.py
@@ -1,12 +1,26 @@
 import logging
 import requests
+from bs4 import BeautifulSoup
 
 logging.basicConfig(filename='scrape.log', level=20)
 logging.info('Initialized logger')
+
+URL = "http://www.planecrashinfo.com/"
 
 def get_page_html(link):
     response = requests.get(link)
     print(response.text)
     return response.text
 
-get_page_html("http://www.planecrashinfo.com/1920/1920.htm")
+
+def get_soup_object(html):
+    return BeautifulSoup(html)
+
+
+def get_data(starting_year, current_year):
+    for year in range(starting_year, current_year+1):
+        html = get_page_html(URL + str(year) + "/" + str(year) + ".htm")
+        soup = get_soup_object(html)
+
+
+get_data(1996, 2000)
